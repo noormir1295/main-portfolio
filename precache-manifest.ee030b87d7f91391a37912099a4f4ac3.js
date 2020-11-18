ab846f9ec383bf82bd64c3f8b86a5dc313a2eb7a
Noor <noormir1295@gmail.com>
2020-11-18 17:11:11 -0500

    Updates

---
+++ precache-manifest.ee030b87d7f91391a37912099a4f4ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c5584d5da93eded4fac232506c5bea",
+    "url": "/main-portfolio/index.html"
+  },
+  {
+    "revision": "e5caada560563d430050",
+    "url": "/main-portfolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "33e6a020f9efaa4e46b3",
+    "url": "/main-portfolio/static/js/2.c5444ead.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/main-portfolio/static/js/2.c5444ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5caada560563d430050",
+    "url": "/main-portfolio/static/js/main.4285c7f2.chunk.js"
+  },
+  {
+    "revision": "fe645a45aa1f31b552ac",
+    "url": "/main-portfolio/static/js/runtime-main.75fe9bbd.js"
+  }
+]);
