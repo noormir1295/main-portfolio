881c475ed1788bf5fcc5b6fd797c6505bc6321ca
Noor <noormir1295@gmail.com>
2020-11-18 18:03:48 -0500

    adding build folder

---
+++ build/precache-manifest.b2496eddfdbb17f5f0e8060a48b46e0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c9cc1e85a8068001ff1f8b09195807",
+    "url": "/noormir1295/main-portfolioindex.html"
+  },
+  {
+    "revision": "a971c33dd463a16a4097",
+    "url": "/noormir1295/main-portfoliostatic/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5201c7dd3d1039a56f16",
+    "url": "/noormir1295/main-portfoliostatic/js/2.e2d67bc9.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/noormir1295/main-portfoliostatic/js/2.e2d67bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a971c33dd463a16a4097",
+    "url": "/noormir1295/main-portfoliostatic/js/main.dcba8b3d.chunk.js"
+  },
+  {
+    "revision": "0d88cf91c11c67e0135a",
+    "url": "/noormir1295/main-portfoliostatic/js/runtime-main.4b90049d.js"
+  }
+]);
